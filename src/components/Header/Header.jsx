d77133f74446d58ad094ccde06f2cd3be6cce889
unknown <lautaropostiglione@gmail.com>
2023-02-23 19:26:39 -0300

    Avance en el componente header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../../styles/Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      {/* --------- Header top --------- */}
+      <div className="header__top">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__left">
+                <span>Need help?</span>
+                <span className="header__top__help">
+                  <i class="ri-phone-fill"></i>
+                  +54 11 6199-1180
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+                <Link to="#" class="d-flex align-items-center gap-1">
+                  <i class="ri-login-circle-line"></i> Login
+                </Link>
+
+                <Link to="#" class="d-flex align-items-center gap-1">
+                  <i class="ri-user-line"></i> Register
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      {/* --------- Header middle --------- */}
+      <div className="header__mid">
+        <Container>
+          <Row>
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home">
+                    <i class="ri-car-line"></i>
+                    <span>
+                      Rent car <br /> Service
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location">
+                <span>
+                  <i class="ri-earth-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Sunday to Friday</h4>
+                  <h6>10am - 7pm</h6>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
